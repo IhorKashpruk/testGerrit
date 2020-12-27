d2a7b0e9db4276abe660912bacabea707986a132
IhorKashpruk <igork1823@gmail.com>
2020-12-27 22:45:06 +0100

    touch and ttttttth Change-Id: If81f6fb52a09037be79415bec724308e12710453

--- src/touch.hpp
+++ src/touch.hpp
@@ -1,10 +1,15 @@
 #pragma once
+#include <iostream>
+#include <string>
 
 #define MAX_TOUCH_ID 230
 
 struct touch
 {
+  void touch_to() {}
 private:
   unsigned int id_;
 };
 #define THE_END_OF_THE_FILE
+
+#define THE_END_OF_THE_CLASS
